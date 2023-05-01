4ad340beaa38f229df320f9846f93fee25eed266
Никита <@DESKTOP-SHP7I2M>
2023-05-02 00:01:22 +0300

    6.1

--- addressbook-web-tests/addressbook-web-tests/tests/GroupCreationTests.cs
+++ addressbook-web-tests/addressbook-web-tests/tests/GroupCreationTests.cs
@@ -1,5 +1,6 @@
 ﻿using NUnit.Framework;
 using System.Collections.Generic;
+using System.IO;
 
 namespace WebAddressbookTests
 {
@@ -15,6 +16,22 @@ namespace WebAddressbookTests
                 {
                     Header = GenerateRandomString(100),
                     Footer = GenerateRandomString(100)
+                });
+            }
+            return groups;
+        }
+
+        public static IEnumerable<GroupData> GroupDataFromFile()
+        {
+            List<GroupData> groups = new List<GroupData>();
+            string[] lines = File.ReadAllLines(@"groups.csv");
+            foreach (string l in lines)
+            {
+                string[] parts = l.Split(',');
+                groups.Add(new GroupData(parts[0])
+                {
+                    Header = parts[1],
+                    Footer = parts[2]
                 });
             }
             return groups;
