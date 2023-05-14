e9aa7cbd7e33ffaadd9b90f7dbf7a154a75a797f
Никита <@DESKTOP-SHP7I2M>
2023-05-14 10:02:28 +0300

    18

---
+++ addressbook_tests_autoit/addressbook_tests_autoit/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("addressbook_tests_autoit")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("addressbook_tests_autoit")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("a758d398-d25f-4668-962d-223698bff5aa")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ addressbook_tests_autoit/addressbook_tests_autoit/tests/GroupDeletionTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace addressbook_tests_autoit
+{
+    [TestFixture]
+    public class GroupDeletionTests : TestBase
+    {
+        [Test]
+        public void TestGroupDeletion()
+        {
+            app.Groups.ConfirmGroupExists();
+            
+            List<GroupData> oldGroups = app.Groups.GetGroupsList();
+
+            GroupData newGroup = new GroupData()
+            {
+                Name = "test"
+            };
+
+            app.Groups.Remove(0);
+
+            List<GroupData> newGroups = app.Groups.GetGroupsList();
+            oldGroups.Remove(newGroup);
+            oldGroups.Sort();
+            newGroups.Sort();
+
+            Assert.AreEqual(oldGroups.Count - 1, newGroups.Count);
+        }
+    }
+}
